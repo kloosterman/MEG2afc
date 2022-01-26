5165eaaa3c6236961f4a0a38759fc27e667a2a0a
Niels Kloosterman <kloosterman@mpib-berlin.mpg.de>
2022-01-26 10:54:20 +0100

    Update MEG2afc_preproc_setup.m

--- preprocessing/MEG2afc_preproc_setup.m
+++ preprocessing/MEG2afc_preproc_setup.m
@@ -104,5 +104,5 @@ if strcmp(backend, 'local')
 end
 
 qsubcellfun(fun2run, cfglist, 'memreq', memreq*1e6, 'timreq', timreq*60, 'stack', stack, ...
-  'StopOnError', true, 'backend', backend, 'options', options, 'StopOnError', false);
+  'StopOnError', false, 'backend', backend, 'options', options);
 
